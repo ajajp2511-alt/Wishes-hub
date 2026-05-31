7a57f28ec2a7ec020fe2a5a674b7de4eb11c2235
Ajaj patel <ajajp2511@gmail.com>
2026-05-31 18:02:32 +0530

    Update verify-pass.js

--- api/verify-pass.js
+++ api/verify-pass.js
@@ -1,14 +1,15 @@
 export default async function handler(req, res) {
-    if (req.method !== 'POST') return res.status(405).json({ message: "Only POST allowed" });
+    if (req.method !== 'POST') return res.status(405).end();
 
     try {
+        // Vercel auto-parse karta hai, isliye JSON.parse mat likhna
         const { password } = req.body; 
         const SECURE_PASS = process.env.ADMIN_PASSWORD;
 
         if (password === SECURE_PASS) {
             return res.status(200).json({ success: true });
         } else {
-            return res.status(401).json({ success: false, message: "Ghalat Password" });
+            return res.status(401).json({ success: false, message: "Ghalat Key!" });
         }
     } catch (error) {
         return res.status(500).json({ success: false, error: error.message });
