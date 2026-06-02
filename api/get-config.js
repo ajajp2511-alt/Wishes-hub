65b853728c3f981faf0768c00fb8c62d069bc102
Ajaj patel <ajajp2511@gmail.com>
2026-06-02 22:31:26 +0530

    Add Cache-Control header to get-config handler

--- api/get-config.js
+++ api/get-config.js
@@ -1,4 +1,5 @@
 export default function handler(req, res) {
+    res.setHeader('Cache-Control', 'no-store, max-age=0'); // Kisi bhi cache bypass ke liye
     res.status(200).json({
         apiKey: process.env.FIREBASE_API_KEY,
         authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -6,6 +7,6 @@ export default function handler(req, res) {
         storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
         messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
         appId: process.env.FIREBASE_APP_ID,
-        adminUid: process.env.ADMIN_UID // Ye line add karein
+        adminUid: process.env.ADMIN_UID
     });
 }
