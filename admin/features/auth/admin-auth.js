60e122805c64533cd297e5c8271dbd306374294f
Ajaj patel <ajajp2511@gmail.com>
2026-06-04 16:14:12 +0530

    Update admin-auth.js

--- admin/features/auth/admin-auth.js
+++ admin/features/auth/admin-auth.js
@@ -1,11 +1,10 @@
 function verifyMasterPassword() {
     const passInput = document.getElementById('admin-pass');
     const statusDiv = document.getElementById('status');
-    const pass = passInput.value.trim();
 
-    if (pass === "1234") {
+    if (passInput.value.trim() === "1234") {
         localStorage.setItem("admin_auth_status", "active");
-        window.location.reload(); // Refresh karke panel dikhayega
+        window.location.reload(); 
     } else {
         statusDiv.innerText = "❌ Ghalat Password!";
         statusDiv.style.color = "red";
