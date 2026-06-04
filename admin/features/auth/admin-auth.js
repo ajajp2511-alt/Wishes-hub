cd76a7b51c3c4bcc5059e0695e202eee9c8cab30
Ajaj patel <ajajp2511@gmail.com>
2026-06-04 19:51:54 +0530

    Update admin-auth.js

--- admin/features/auth/admin-auth.js
+++ admin/features/auth/admin-auth.js
@@ -6,9 +6,17 @@ window.verifyMasterPassword = function() {
 
     if (passInput.value.trim() === "1234") {
         localStorage.setItem("admin_auth_status", "active");
-        window.location.reload();
+        window.location.reload(); 
     } else {
         statusDiv.innerText = "❌ Ghalat Password!";
         statusDiv.style.color = "red";
     }
 }
+
+// Button par click event lagana
+document.addEventListener("DOMContentLoaded", function() {
+    const unlockBtn = document.getElementById('unlock-btn');
+    if(unlockBtn) {
+        unlockBtn.addEventListener('click', verifyMasterPassword);
+    }
+});
