02bd5a240cb7ce10e02e8fac7825001bf5519e5c
Ajaj patel <ajajp2511@gmail.com>
2026-06-04 19:37:35 +0530

    Update admin-auth.js

--- admin/features/auth/admin-auth.js
+++ admin/features/auth/admin-auth.js
@@ -1,13 +1,14 @@
-function verifyMasterPassword() {
+window.verifyMasterPassword = function() {
     const passInput = document.getElementById('admin-pass');
     const statusDiv = document.getElementById('status');
 
+    if (!passInput || !statusDiv) return;
+
     if (passInput.value.trim() === "1234") {
         localStorage.setItem("admin_auth_status", "active");
-        window.location.reload(); 
+        window.location.reload();
     } else {
         statusDiv.innerText = "❌ Ghalat Password!";
         statusDiv.style.color = "red";
     }
 }
-window.verifyMasterPassword = verifyMasterPassword;
