c0bb1505c94bebd51a915d2a15ffca9467069c23
Ajaj patel <ajajp2511@gmail.com>
2026-07-19 23:21:19 +0530

    Update wishes-assembly.js

--- public/features/wishes-feed/wishes-assembly.js
+++ public/features/wishes-feed/wishes-assembly.js
@@ -1,13 +1,6 @@
-// public/features/wishes-feed/wishes-assembly.js
 import WishesCore from './wishes-core.js';
 
 export default async function initWishesFeed() {
     console.log("Wishes-Feed: Initializing...");
-    try {
-        // Humne yahan await lagaya hai taaki data load hone ka wait ho
-        await WishesCore.loadWishes();
-        console.log("Wishes-Feed: Loaded successfully.");
-    } catch (err) {
-        console.error("Wishes-Feed: Failed to load", err);
-    }
+    await WishesCore.loadWishes();
 }
