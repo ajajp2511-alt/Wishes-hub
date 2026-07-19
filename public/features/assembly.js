fb28282af464bd2b82c903184e4807dcd98b94ab
Ajaj patel <ajajp2511@gmail.com>
2026-07-19 19:12:49 +0530

    Create assembly.js

---
+++ public/features/assembly.js
@@ -0,0 +1,11 @@
+// Wishes-hub Feature Aggregator
+import initLocalization from './localization-manager/lang-assembly.js';
+
+const Assembly = {
+    initAll() {
+        initLocalization();
+        // Aage yahan Wishes-hub ke aur features add karenge
+    }
+};
+
+export default Assembly;
