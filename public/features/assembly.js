208e6c1b07674ae5eae519a502c474ef305330c9
Ajaj patel <ajajp2511@gmail.com>
2026-07-19 20:38:40 +0530

    Update assembly.js

--- public/features/assembly.js
+++ public/features/assembly.js
@@ -2,20 +2,17 @@ import initStorageManager from './storage-manager/storage-assembly.js';
 import initThemeEngine from './theme-engine/theme-assembly.js';
 import initLocalization from './localization-manager/lang-assembly.js';
 import initCategories from './categories-manager/categories-assembly.js';
-import initWishesFeed from './wishes-feed/wishes-assembly.js'; // Naya import
+import initWishesFeed from './wishes-feed/wishes-assembly.js';
+import initUiLayout from './ui-layout/ui-assembly.js'; // Naya Import
 
 const Assembly = {
     initAll() {
-        console.log("Wishes-hub Engine Initializing...");
-
         initStorageManager();
         initLocalization();
         initThemeEngine();
+        initUiLayout(); // Sabse pehle UI load karein
         initCategories();
-        initWishesFeed(); // Registering the feed
-
-        console.log("Wishes-hub Engine Started Successfully.");
+        initWishesFeed();
     }
 };
-
 export default Assembly;
