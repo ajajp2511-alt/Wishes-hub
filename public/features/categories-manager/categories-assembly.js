2ea40a6b2898b945b94af70e61750c4d86c8ab4d
Ajaj patel <ajajp2511@gmail.com>
2026-07-19 20:01:14 +0530

    Refactor initCategories to use CategoriesCore
    Refactored initCategories function to use CategoriesCore for loading categories instead of CategoriesMain.

--- public/features/categories-manager/categories-assembly.js
+++ public/features/categories-manager/categories-assembly.js
@@ -1,22 +1,5 @@
-import CategoriesMain from './categories-main.js';
+import CategoriesCore from './categories-core.js';
 
-export default function initCategories(allWishesData, onFilterCallback) {
-    const selector = '.categories-container-wrapper';
-    
-    // Buttons ke naam
-    const myCategories = ['Text wishes', 'Image wishes'];
-
-    CategoriesMain.init(selector, allWishesData, (selectedTag) => {
-        if (selectedTag === 'all') {
-            onFilterCallback(allWishesData);
-        } else if (selectedTag === 'Text wishes') {
-            // Sirf wahi wishes jahan image null ya empty hai
-            const filtered = allWishesData.filter(wish => !wish.image);
-            onFilterCallback(filtered);
-        } else if (selectedTag === 'Image wishes') {
-            // Sirf wahi wishes jahan image field mein link hai
-            const filtered = allWishesData.filter(wish => wish.image && wish.image.length > 5);
-            onFilterCallback(filtered);
-        }
-    }, myCategories);
+export default function initCategories() {
+    CategoriesCore.loadCategories();
 }
