fc5b25955cd03e4fbda3e00097fe0cf6d488246e
Ajaj patel <ajajp2511@gmail.com>
2026-07-19 19:50:30 +0530

    Update assembly.js

--- public/features/assembly.js
+++ public/features/assembly.js
@@ -1,11 +1,12 @@
-// Updated Wishes-hub: assembly.js
 import initLocalization from './localization-manager/lang-assembly.js';
 import initThemeEngine from './theme-engine/theme-assembly.js';
+import initStorageManager from './storage-manager/storage-assembly.js';
 
 const Assembly = {
     initAll() {
+        initStorageManager(); // Storage sabse pehle load hona chahiye
         initLocalization();
-        initThemeEngine(); // Naya feature added
+        initThemeEngine();
     }
 };
 
