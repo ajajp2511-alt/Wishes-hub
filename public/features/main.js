827c0910f3e83cfcb760187be9a65cad3d4e4e8b
Ajaj patel <ajajp2511@gmail.com>
2026-07-19 19:12:02 +0530

    Create main.js

---
+++ public/features/main.js
@@ -0,0 +1,11 @@
+// Wishes-hub Master Entry Point
+import Assembly from './assembly.js';
+
+const Main = {
+    init() {
+        console.log("Wishes-hub Engine Initializing...");
+        Assembly.initAll();
+    }
+};
+
+export default Main;
