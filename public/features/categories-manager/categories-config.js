0c429d22173ba0763abfa3a789fbc328ac7b9647
Ajaj patel <ajajp2511@gmail.com>
2026-07-19 12:27:12 +0530

    Update categories-config.js

--- public/features/categories-manager/categories-config.js
+++ public/features/categories-manager/categories-config.js
@@ -1,1 +1,13 @@
-
+const CategoriesConfig = {
+    list: [
+        { id: "motivational", name: "Motivational", tag: "motivational" },
+        { id: "shayari", name: "Shayari", tag: "shayari" },
+        { id: "festival", name: "Festival", tag: "festival" },
+        { id: "special", name: "Special", tag: "special" },
+        { id: "true-line", name: "True Line", tag: "true-line" },
+        { id: "text-wishes", name: "Text", tag: "text" },
+        { id: "image-wishes", name: "Image", tag: "image" },
+        { id: "heart-touching", name: "Heart Touching", tag: "heart-touching" }
+    ]
+};
+export default CategoriesConfig;
