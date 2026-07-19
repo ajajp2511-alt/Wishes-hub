77d3ec06a775b3244bc1bab7a34a1f49223538b8
Ajaj patel <ajajp2511@gmail.com>
2026-07-19 19:13:51 +0530

    Create bus.js

---
+++ public/features/core-bus/bus.js
@@ -0,0 +1,15 @@
+// Wishes-hub Core-Bus
+const CoreBus = {
+    listeners: {},
+    emit(event, data) {
+        if (this.listeners[event]) {
+            this.listeners[event].forEach(callback => callback(data));
+        }
+    },
+    on(event, callback) {
+        if (!this.listeners[event]) this.listeners[event] = [];
+        this.listeners[event].push(callback);
+    }
+};
+
+export default CoreBus;
