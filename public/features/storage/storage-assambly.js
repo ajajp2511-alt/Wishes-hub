f348674ffe60b3f216121c777aeacf0cea2042e1
Ajaj patel <ajajp2511@gmail.com>
2026-07-19 19:48:40 +0530

    Create storage-assambly.js

---
+++ public/features/storage/storage-assambly.js
@@ -0,0 +1,7 @@
+// Wishes-hub: Storage Assembly
+import StorageCore from './storage-core.js';
+
+export default function initStorageManager() {
+    console.log("Wishes-hub Storage Manager Ready.");
+    return StorageCore;
+}
