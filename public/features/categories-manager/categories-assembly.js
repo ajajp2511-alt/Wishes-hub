daba01b08a9f15568d1370e863ea41f8c65c1421
Ajaj patel <ajajp2511@gmail.com>
2026-07-20 08:39:08 +0530

    Update categories-assembly.js

--- public/features/categories-manager/categories-assembly.js
+++ public/features/categories-manager/categories-assembly.js
@@ -1,12 +1,5 @@
-import CategoriesCore from './categories-core.js';
+import CategoriesCore from '/public/features/categories-manager/categories-core.js';
 
-export default async function initCategories() {
-    console.log("Categories: Initializing...");
-    try {
-        // await zaroori hai taaki Assembly ko pata chale kab kaam khatam hua
-        await CategoriesCore.loadCategories();
-        console.log("Categories: Loaded successfully.");
-    } catch (err) {
-        console.error("Categories: Failed to load", err);
-    }
+export default function initCategories() {
+    CategoriesCore.render();
 }
