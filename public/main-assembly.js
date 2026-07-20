6b3aa804de215bdac2170ce450ad873c9567a4df
Ajaj patel <ajajp2511@gmail.com>
2026-07-20 19:34:32 +0530

    Update main-assembly.js

--- public/main-assembly.js
+++ public/main-assembly.js
@@ -1,15 +1,16 @@
-import initStorageManager from './features/storage-manager/storage-assembly.js';
-import initDebugger from './features/debugger-manager/debug-assembly.js';
-import initUiLayout from './features/ui-layout/ui-assembly.js';
-import initPageManager from './features/page/page-assembly.js';
-import initFeeders from './features/feeders/feeders-assembly.js';
-import initShareManager from './features/share/share-assembly.js';
-import initNotification from './features/notification-manager/notify-assembly.js';
-import initInteraction from './features/interaction/interact-assembly.js';
-import initAnalytics from './features/analytics/analytics-assembly.js';
-import initUserProfile from './features/user-profile/profile-assembly.js';
-import initOfflineManager from './features/offline-manager/offline-assembly.js';
-import initWishOfTheDay from './features/wish-of-the-day/wish-assembly.js';
+// Path mein ../ ka use karein taaki public folder se bahar nikal kar features folder mil sake
+import initStorageManager from '../features/storage-manager/storage-assembly.js';
+import initDebugger from '../features/debugger-manager/debug-assembly.js';
+import initUiLayout from '../features/ui-layout/ui-assembly.js';
+import initPageManager from '../features/page/page-assembly.js';
+import initFeeders from '../features/feeders/feeders-assembly.js';
+import initShareManager from '../features/share/share-assembly.js';
+import initNotification from '../features/notification-manager/notify-assembly.js';
+import initInteraction from '../features/interaction/interact-assembly.js';
+import initAnalytics from '../features/analytics/analytics-assembly.js';
+import initUserProfile from '../features/user-profile/profile-assembly.js';
+import initOfflineManager from '../features/offline-manager/offline-assembly.js';
+import initWishOfTheDay from '../features/wish-of-the-day/wish-assembly.js';
 
 async function runSafe(func, moduleName) {
     try {
