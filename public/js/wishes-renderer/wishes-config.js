c517dd65774565052a98d56d1f550161ea7522ae
Ajaj patel <ajajp2511@gmail.com>
2026-07-21 20:47:48 +0530

    Create wishes-config.js

---
+++ public/js/wishes-renderer/wishes-config.js
@@ -0,0 +1,7 @@
+/* Wishes Renderer Configuration */
+export const WishesConfig = {
+    apiEndpoint: '/api/get-wishes',
+    mediaProxyBase: 'https://imtqy.com/bot',
+    defaultContainerId: 'latest-wishes-section',
+    fallbackContainerId: 'wishes-grid'
+};
