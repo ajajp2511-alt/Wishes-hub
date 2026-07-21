06d561ff88153a666c12d566bd8f8fe2752aeb99
Ajaj patel <ajajp2511@gmail.com>
2026-07-21 20:33:44 +0530

    Create ads-config.js

---
+++ public/js/ads-manager/ads-config.js
@@ -0,0 +1,10 @@
+/* Ads Manager Configuration */
+export const AdsConfig = {
+    provider: 'google-ads',
+    enabled: true,
+    refreshInterval: 30000, // 30 seconds
+    slots: {
+        banner: 'banner-slot-1',
+        sidebar: 'sidebar-slot-1'
+    }
+};
