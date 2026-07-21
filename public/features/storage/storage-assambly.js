4bb22a67252753471da87f99dbe9f24b122d5adf
Ajaj patel <ajajp2511@gmail.com>
2026-07-21 08:44:03 +0530

    Update storage-assambly.js

--- public/features/storage/storage-assambly.js
+++ public/features/storage/storage-assambly.js
@@ -1,7 +1,1 @@
-// Wishes-hub: Storage Assembly
-import StorageCore from './storage-core.js';
-
-export default function initStorageManager() {
-    console.log("Wishes-hub Storage Manager Ready.");
-    return StorageCore;
-}
+successful
