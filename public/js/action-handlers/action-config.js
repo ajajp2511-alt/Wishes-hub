192b85996b6af53203d428cd16c2d71fbc5cbc9f
Ajaj patel <ajajp2511@gmail.com>
2026-07-21 20:28:13 +0530

    Create action-config.js

---
+++ public/js/action-handlers/action-config.js
@@ -0,0 +1,9 @@
+/* Action Handlers Configuration */
+export const ActionConfig = {
+    debounceTime: 300,
+    animationDuration: 200,
+    selectors: {
+        triggerClass: '.action-btn',
+        activeClass: 'active'
+    }
+};
