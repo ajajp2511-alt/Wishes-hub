b3cd605a77fd8c02b4eaf7f4507c574b00a47eb6
Ajaj patel <ajajp2511@gmail.com>
2026-07-25 07:32:29 +0530

    Create music-config.js

---
+++ public/js/music-player/music-config.js
@@ -0,0 +1,11 @@
+export const MUSIC_CONFIG = {
+  SELECTORS: {
+    AUDIO: '#bg-audio',
+    PLAY_PAUSE_BTN: '#music-play-pause',
+    VOLUME_SLIDER: '#music-volume',
+    PROGRESS_BAR: '#music-progress',
+    TRACK_TITLE: '#music-track-title'
+  },
+  DEFAULT_VOLUME: 0.5,
+  AUTOPLAY: false
+};
