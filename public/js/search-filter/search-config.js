598988a8f53ddda5613faca2dc8cb2d89292d396
Ajaj patel <ajajp2511@gmail.com>
2026-07-25 07:36:54 +0530

    Create search-config.js

---
+++ public/js/search-filter/search-config.js
@@ -0,0 +1,10 @@
+export const SEARCH_CONFIG = {
+  SELECTORS: {
+    SEARCH_INPUT: '#search-input',
+    CATEGORY_FILTER: '.category-btn',
+    RESULTS_CONTAINER: '#wishes-list',
+    CLEAR_BTN: '#search-clear-btn'
+  },
+  DEBOUNCE_DELAY: 300, // ms
+  ACTIVE_CATEGORY_CLASS: 'active'
+};
