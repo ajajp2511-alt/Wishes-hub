e1a4c6d438613666875ceb15a55fe87387aec43a
Ajaj patel <ajajp2511@gmail.com>
2026-07-25 07:27:23 +0530

    Create dark-core.js

---
+++ public/js/dark-mode/dark-core.js
@@ -0,0 +1,24 @@
+import { DARK_MODE_CONFIG } from './dark-config.js';
+
+export class DarkModeCore {
+  constructor() {
+    this.currentTheme = localStorage.getItem(DARK_MODE_CONFIG.STORAGE_KEY) || DARK_MODE_CONFIG.THEMES.LIGHT;
+  }
+
+  getTheme() {
+    return this.currentTheme;
+  }
+
+  toggleTheme() {
+    this.currentTheme = this.currentTheme === DARK_MODE_CONFIG.THEMES.DARK 
+      ? DARK_MODE_CONFIG.THEMES.LIGHT 
+      : DARK_MODE_CONFIG.THEMES.DARK;
+      
+    localStorage.setItem(DARK_MODE_CONFIG.STORAGE_KEY, this.currentTheme);
+    return this.currentTheme;
+  }
+
+  isDark() {
+    return this.currentTheme === DARK_MODE_CONFIG.THEMES.DARK;
+  }
+}
