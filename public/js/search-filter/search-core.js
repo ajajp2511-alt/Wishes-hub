2c40af367d08ff978f60634d8a4010478d5b16e4
Ajaj patel <ajajp2511@gmail.com>
2026-07-25 07:37:29 +0530

    Create search-core.js

---
+++ public/js/search-filter/search-core.js
@@ -0,0 +1,37 @@
+import { SEARCH_CONFIG } from './search-config.js';
+
+export class SearchCore {
+  constructor(items = []) {
+    this.items = items; // Master data list
+    this.currentQuery = '';
+    this.currentCategory = 'all';
+  }
+
+  setItems(items) {
+    this.items = items;
+  }
+
+  filterData(query = this.currentQuery, category = this.currentCategory) {
+    this.currentQuery = query.toLowerCase().trim();
+    this.currentCategory = category;
+
+    return this.items.filter(item => {
+      const matchesQuery = !this.currentQuery || 
+        (item.title && item.title.toLowerCase().includes(this.currentQuery)) ||
+        (item.tags && item.tags.some(tag => tag.toLowerCase().includes(this.currentQuery)));
+
+      const matchesCategory = this.currentCategory === 'all' || 
+        (item.category && item.category.toLowerCase() === this.currentCategory.toLowerCase());
+
+      return matchesQuery && matchesCategory;
+    });
+  }
+
+  debounce(func, delay = SEARCH_CONFIG.DEBOUNCE_DELAY) {
+    let timeoutId;
+    return (...args) => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => func.apply(this, args), delay);
+    };
+  }
+      }
