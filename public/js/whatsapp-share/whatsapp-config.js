fe70040611c4dde85a197f7cabc2ffd9eb99a83b
Ajaj patel <ajajp2511@gmail.com>
2026-07-25 07:41:10 +0530

    Create whatsapp-config.js

---
+++ public/js/whatsapp-share/whatsapp-config.js
@@ -0,0 +1,8 @@
+export const WHATSAPP_CONFIG = {
+  SELECTORS: {
+    SHARE_BTN: '.whatsapp-share-btn',
+    NAME_INPUT: '#sender-name-input'
+  },
+  BASE_URL: 'https://api.whatsapp.com/send',
+  DEFAULT_TEXT: 'Look at this special wish for you!'
+};
