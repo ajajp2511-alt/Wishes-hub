75e5dcce0ae7cdb18c290ed6369116b4a577159b
Ajaj patel <ajajp2511@gmail.com>
2026-07-25 07:26:39 +0530

    Create dark-config.js

---
+++ public/js/dark-mode/dark-config.js
@@ -0,0 +1,11 @@
+export const DARK_MODE_CONFIG = {
+  STORAGE_KEY: 'wishes_hub_theme',
+  THEMES: {
+    DARK: 'dark',
+    LIGHT: 'light'
+  },
+  SELECTORS: {
+    TOGGLE_BTN: '#dark-mode-toggle',
+    BODY_CLASS: 'dark-theme'
+  }
+};
