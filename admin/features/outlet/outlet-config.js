d28237b4db331def96a8a0496d2b348d25e88b6b
Ajaj patel <ajajp2511@gmail.com>
2026-08-06 09:31:53 +0530

    Update outlet-config.js

--- admin/features/outlet/outlet-config.js
+++ admin/features/outlet/outlet-config.js
@@ -1,5 +1,5 @@
 export const OUTLET_CONFIG = {
     ROOT_CONTAINER_ID: 'dynamic-content-root',
-    DEFAULT_FEATURE: 'analytics',
+    DEFAULT_FEATURE: null,
     BASE_PATH: '/admin/features/outlet'
 };
