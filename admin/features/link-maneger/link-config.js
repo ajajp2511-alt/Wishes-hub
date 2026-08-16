85b3a089c5f436eadeaf435ecea0adaf0f5c6d21
Ajaj patel <ajajp2511@gmail.com>
2026-08-16 22:44:40 +0530

    Create link-config.js

---
+++ admin/features/link-maneger/link-config.js
@@ -0,0 +1,12 @@
+/**
+ * Link Manager Configuration
+ * Path: admin/features/link-manager/link-config.js
+ */
+
+export const LINK_CONFIG = {
+  version: '1.0.0',
+  defaultDomain: 'hub.link',
+  enableDeepLinking: true,
+  enablePixelRetargeting: true,
+  enableBotProtection: true
+};
