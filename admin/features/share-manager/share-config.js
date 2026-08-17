803988e979f5a3ac93d2e1ced6680654fece0648
Ajaj patel <ajajp2511@gmail.com>
2026-08-17 12:33:22 +0530

    Create share-config.js

---
+++ admin/features/share-manager/share-config.js
@@ -0,0 +1,12 @@
+/**
+ * Share Manager Configuration
+ * Path: admin/features/share-manager/share-config.js
+ */
+
+export const SHARE_CONFIG = {
+  version: '1.0.0',
+  defaultLanguage: 'hi',
+  enableViralityTracking: true,
+  enableGamificationRewards: true,
+  defaultAppUrl: 'https://hub.vercel.app'
+};
