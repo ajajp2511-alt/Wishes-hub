c6035c6c7e6e0f89f8bd6f2638df3a6864f475af
Ajaj patel <ajajp2511@gmail.com>
2026-08-29 15:17:25 +0530

    Create setting-nav.js

---
+++ admin/features/menu-navigation/items/setting-nav.js
@@ -0,0 +1,10 @@
+export const settingNavItem = {
+  id: 'setting',
+  label: 'Settings',
+  icon: '⚙️',
+  subItems: [
+    { id: 'setting-seo', label: 'SEO & Meta' },
+    { id: 'setting-ads', label: 'AdSense & Ads Config' },
+    { id: 'setting-theme', label: 'Theme & Custom CSS' }
+  ]
+};
