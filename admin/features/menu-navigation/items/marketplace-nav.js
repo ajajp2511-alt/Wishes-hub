b716ddcf207885354ddfdec1cc42309c61390b0a
Ajaj patel <ajajp2511@gmail.com>
2026-08-29 14:54:31 +0530

    Add marketplace navigation item with sub-items

---
+++ admin/features/menu-navigation/items/marketplace-nav.js
@@ -0,0 +1,10 @@
+export const marketplaceNavItem = {
+  id: 'marketplace',
+  label: 'Marketplace & Creators',
+  icon: '🏪',
+  subItems: [
+    { id: 'market-store', label: 'Premium Templates Store' },
+    { id: 'market-creators', label: 'Creator Submissions' },
+    { id: 'market-payouts', label: 'Payouts & Commissions' }
+  ]
+};
