eefd959bb1a929816fc228a35b0bbec5222a024b
Ajaj patel <ajajp2511@gmail.com>
2026-09-06 15:52:50 +0530

    Create omni-config.js

---
+++ admin/features/omni-platform-exporter/omni-config.js
@@ -0,0 +1,6 @@
+PLATFORM_SUITE_VERSION: 'v4.0.0-omni',
+    SUPPORTED_TARGETS: ['Android', 'iOS', 'Windows', 'macOS', 'Linux', 'Web PWA'],
+    CLOUD_BUILD_PROVIDER: 'WishesHub Secure Build Farm',
+    DEFAULT_OTA_CHANNEL: 'Production Stable',
+    LOCALIZATION_DEFAULT: 'en'
+};
