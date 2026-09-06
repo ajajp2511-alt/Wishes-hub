8117f3a2d650533f09637cd58b36c405e166aff4
Ajaj patel <ajajp2511@gmail.com>
2026-09-06 11:54:03 +0530

    Create geo-config.js

---
+++ admin/features/geo-fencing-manager/geo-config.js
@@ -0,0 +1,13 @@
+export const GEO_CONFIG = {
+    ENGINE_VERSION: 'v3.0.0-enterprise',
+    DEFAULT_POLICY: 'Allow',
+    VPN_DETECTION_STRICTNESS: 'Maximum',
+    AI_PREDICTIVE_SHIELD_ENABLED: true,
+    HONEYPOT_MODE: 'Active',
+    DEFAULT_CURRENCY_MAP: {
+        'IN': 'INR',
+        'US': 'USD',
+        'GB': 'GBP',
+        'EU': 'EUR'
+    }
+};
