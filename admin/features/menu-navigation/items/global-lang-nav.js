e88506264bf72dce9f630806508d5b9dae3db228
Ajaj patel <ajajp2511@gmail.com>
2026-09-08 14:09:59 +0530

    Create global-lang-nav.js

---
+++ admin/features/menu-navigation/items/global-lang-nav.js
@@ -0,0 +1,14 @@
+/**
+ * Global Language & Direction Nav Item
+ * Path: admin/features/menu-navigation/items/global-lang-nav.js
+ */
+
+export const globalLangNavItem = {
+  id: 'global-lang-section',
+  label: 'Global Languages',
+  icon: '🌐',
+  subItems: [
+    { id: 'lang-settings', label: 'Languages & Direction' },
+    { id: 'lang-translations', label: 'Translation Sync' }
+  ]
+};
