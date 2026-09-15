1f04dcb640b3baab38a571c63ec8879e37cd6e1c
Ajaj patel <ajajp2511@gmail.com>
2026-09-15 13:53:02 +0530

    Update manage-loading-history.js

--- admin/features/manage-loading/modules/manage-loading-history.js
+++ admin/features/manage-loading/modules/manage-loading-history.js
@@ -1,1 +1,9 @@
-
+/**
+ * modules/manage-loading-history.js
+ * Session state logging & queue management helper
+ */
+export const LoadingHistory = {
+  initSessionTracking() {
+    window.__wishes_loader_history = window.__wishes_loader_history || [];
+  }
+};
