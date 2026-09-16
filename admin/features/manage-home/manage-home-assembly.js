cd68e2a9e8432d9a6a98c0886a4c71c06d5a97a3
Ajaj patel <ajajp2511@gmail.com>
2026-09-16 22:51:53 +0530

    Update manage-home-assembly.js

--- admin/features/manage-home/manage-home-assembly.js
+++ admin/features/manage-home/manage-home-assembly.js
@@ -9,11 +9,14 @@ import { HomeActions } from './modules/manage-home-actions.js';
 import { HomeAnnouncements } from './modules/manage-home-announcements.js';
 
 export function assembleHomeModule(containerSelector) {
+  // 1. Sabse pehle core dashboard aur DOM containers render karein
+  homeDashboard.init(containerSelector);
+
+  // 2. Uske baad sub-modules ko call karein taaki elements available hon
   HomeAnnouncements.render();
   HomeStats.fetchAndRender();
   HomeActivity.initStream();
   HomeActions.bindShortcuts();
   
-  homeDashboard.init(containerSelector);
   return homeDashboard;
 }
