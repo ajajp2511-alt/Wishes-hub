a8933bd0f2031f2335bfdabee6f1c86e280acc03
Ajaj patel <ajajp2511@gmail.com>
2026-09-20 14:38:57 +0530

    Create user-permissions-nav.js

---
+++ admin/features/menu-navigation/items/user-permissions-nav.js
@@ -0,0 +1,10 @@
+export const userPermissionsNavItem = {
+    id: 'menu-user-permissions',
+    label: 'User Permissions',
+    icon: '👤',
+    subItems: [
+        { id: 'user-perm-types', label: 'Permission Types' },
+        { id: 'user-perm-config', label: 'Default Config' },
+        { id: 'user-perm-messages', label: 'Messages' }
+    ]
+};
